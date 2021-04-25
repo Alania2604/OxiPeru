15cfc9de0947ea4e5ab6b6c79984d66e925ceb58
Luis <luis_qr_05@hotmail.com>
2021-04-25 17:56:48 -0500

    cambios en repository

--- src/main/java/com/oxiperu/appweb/repository/contactoRepository.java
+++ src/main/java/com/oxiperu/appweb/repository/contactoRepository.java
@@ -1,6 +1,14 @@
 package com.oxiperu.appweb.repository;
 
 public class ContactoRepository{
+    int nump;
+    
+    public void setNump(Integer nump){
+        this.nump=nump;
+    }
+    public Integer getNump(){
+        return this.nump;
+    }
 
     
 }
