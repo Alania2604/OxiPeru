7728e92615d7de404ea95c3b40c0c5433d9a85da
JohnEMG1992 <john.maldonado.gonzales@gmail.com>
2021-04-26 02:49:38 -0500

    modelocontacto

--- src/main/java/com/oxiperu/appweb/model/contacto.java
+++ src/main/java/com/oxiperu/appweb/model/contacto.java
@@ -7,5 +7,44 @@ package com.oxiperu.appweb.model;
 
 public class contacto{
 
+    private String nombre;
+    private String contraseña;
+    private String email;
+    private String dni;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return this.contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return this.dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+
+
     
 }
