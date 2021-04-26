e8e46c0c1a44f535734609e0a354be025c13224b
JohnEMG1992 <john.maldonado.gonzales@gmail.com>
2021-04-26 05:41:34 -0500

    flujo de formulario contacto y contacto controller

--- src/main/java/com/oxiperu/appweb/controller/ContactoController.java
+++ src/main/java/com/oxiperu/appweb/controller/ContactoController.java
@@ -1,6 +1,26 @@
 package com.oxiperu.appweb.controller;
 
+import com.oxiperu.appweb.model.contacto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class ContactoController{
 
+    @GetMapping("/contacto")
+    public String contacto(Model model){
+        contacto contacto =new contacto();
+        model.addAttribute("contacto", contacto);
+        return "contacto/index";
+    }
+
+    @PostMapping("/contacto/create")
+    public String postSubmitForm(Model model){
+        model.addAttribute("mensaje", "se registro un contacto");
+        return "contacto/index.html";
+    }
     
 }
