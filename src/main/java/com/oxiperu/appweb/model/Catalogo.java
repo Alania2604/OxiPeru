0bceb3eeca14859805c27f6148de10f6ee59d16c
Shirley Alania <shirley.alarmy@gmail.com>
2021-04-29 00:27:47 -0500

    avance3

--- src/main/java/com/oxiperu/appweb/model/Catalogo.java
+++ src/main/java/com/oxiperu/appweb/model/Catalogo.java
@@ -1,6 +1,32 @@
 package com.oxiperu.appweb.model;
 import java.math.BigDecimal;
+import javax.validation.contraints.NotNull;
+
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import java.lang.invoke.InjectedProfile;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.*;
+
+@Getter
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name="t_catalogo")
 public class Catalogo{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long idCatalogo;
     private BigDecimal precio;
     private String nombre;
