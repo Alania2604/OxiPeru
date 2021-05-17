eeafe7dafdabbd27187d2f72b516026e51d4be2a
JohnEMG1992 <john.maldonado.gonzales@gmail.com>
2021-05-16 19:10:23 -0500

    cambio contact

--- src/main/java/com/oxiperu/appweb/repository/contactoRepository.java
+++ src/main/java/com/oxiperu/appweb/repository/contactoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ContactoRepository extends JpaRepository<Contacto, Integer> {
+public interface  ContactoRepository extends JpaRepository<Contacto, Integer> {
     
 }
