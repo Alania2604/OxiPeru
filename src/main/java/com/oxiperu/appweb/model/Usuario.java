fc53f7a47fff816369e8c9c401122fe4b0f5e69a
JohnEMG1992 <john.maldonado.gonzales@gmail.com>
2021-05-16 20:04:22 -0500

    cambio

--- src/main/java/com/oxiperu/appweb/model/Usuario.java
+++ src/main/java/com/oxiperu/appweb/model/Usuario.java
@@ -19,14 +19,6 @@ public class Usuario {
     @Column(name = "user_id")
     private String userID;
     private String password;
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getUserID() {
-        return null;
-    }
+
 
 }
