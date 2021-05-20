371d0b8facd55c68f7c5296035b5a4a000aeae8d
Luis <luis_qr_05@hotmail.com>
2021-05-20 16:32:35 -0500

    cambios contacto

--- src/main/java/com/oxiperu/appweb/model/Usuario.java
+++ src/main/java/com/oxiperu/appweb/model/Usuario.java
@@ -3,7 +3,7 @@ package com.oxiperu.appweb.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
-
+import javax.persistence.Column;
 
 import lombok.*;
 
@@ -17,7 +17,21 @@ import lombok.*;
 
 public class Usuario {
     @Id
+    @Column(name = "user_id")
         private String userID;
-        
+        private String password;
 
+        public String getUserID() {
+            return password;
+        }
+        public void setUserID(String userID) {
+            this.password = userID;
+        }
+        
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
 }
