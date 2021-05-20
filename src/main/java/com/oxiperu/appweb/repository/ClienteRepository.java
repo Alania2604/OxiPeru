7110262c7c6029471b5cc00cc414d9b1c5f7d9fe
Shirley Alania <shirley.alarmy@gmail.com>
2021-05-20 17:41:50 -0500

    clienteRepository

---
+++ src/main/java/com/oxiperu/appweb/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.oxiperu.appweb.repository;
+
+import com.oxiperu.appweb.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+    
+}
