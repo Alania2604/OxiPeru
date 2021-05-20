a92e523ac447ce594f2643ca5df4738c32439261
Shirley Alania <shirley.alarmy@gmail.com>
2021-05-20 18:19:40 -0500

    producto

--- src/main/java/com/oxiperu/appweb/model/producto.java
+++ src/main/java/com/oxiperu/appweb/model/producto.java
@@ -31,7 +31,7 @@ public class Producto {
     private Date dueDate; //-------------------------
     private String imageFileName;
     private String status;
-/*
+
    public String getDescripcion() {
         return descripcion;
     }
@@ -56,6 +56,6 @@ public class Producto {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-*/
+
 }
 
