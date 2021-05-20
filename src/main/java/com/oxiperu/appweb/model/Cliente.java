cfc136258cc67e8736481c068924bbfbf47b4a24
Shirley Alania <shirley.alarmy@gmail.com>
2021-05-20 17:40:44 -0500

    cliente

---
+++ src/main/java/com/oxiperu/appweb/model/Cliente.java
@@ -0,0 +1,77 @@
+package com.oxiperu.appweb.model;
+
+import  java.util.Date ;
+import  org.springframework.format.annotation.DateTimeFormat ;
+
+import  javax.persistence.Entity ;
+import  javax.persistence.Table ;
+import  javax.persistence.Id ;
+import  javax.persistence.FetchType ;
+import  javax.persistence.OneToOne ;
+import  javax.persistence.GeneratedValue ;
+import  javax.persistence.GenerationType ;
+import  javax.persistence.JoinColumn ;
+
+import  lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "t_customer")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone; 
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date birthdate; //-------------------------
+    private String gender;
+    private String maritalStatus;  
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Usuario user;
+
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    } 
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }     
+}
