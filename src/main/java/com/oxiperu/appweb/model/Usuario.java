60a12d617c0d4ad937baadff71a839b1e02b3d10
Shirley Alania <shirley.alarmy@gmail.com>
2021-05-20 17:41:37 -0500

    usuario

--- src/main/java/com/oxiperu/appweb/model/Usuario.java
+++ src/main/java/com/oxiperu/appweb/model/Usuario.java
@@ -18,7 +18,25 @@ public class Usuario {
    
     @Id
     @Column(name = "user_id")
+<<<<<<< HEAD
     private String userID;
     private String password;
+=======
+        private String userID;
+        private String password;
+>>>>>>> 6fb763e1f02d309575806a3ec4730b6bb69d4f1c
 
+        public String getUserID() {
+            return password;
+        }
+        public void setUserID(String userID) {
+            this.password = userID;
+        }
+        
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
 }
