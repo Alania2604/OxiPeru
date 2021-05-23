07698987e7247376e9b60e2d578d5a8f70a397e9
Shirley Alania <shirley.alarmy@gmail.com>
2021-05-22 23:07:46 -0500

    cambio producto

--- src/main/java/com/oxiperu/appweb/controller/ProductoController.java
+++ src/main/java/com/oxiperu/appweb/controller/ProductoController.java
@@ -7,11 +7,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.oxiperu.appweb.model.Producto;
-//import com.oxiperu.appweb.repository.ProductoRepository;
+import com.oxiperu.appweb.repository.ProductoRepository;
 
 import javax.validation.Valid;
 
-//import java.util.List;
+import java.util.List;
 
 @Controller
 public class ProductoController {
@@ -20,14 +20,18 @@ public class ProductoController {
     private static final String VIEW_CREATE="producto/create"; 
     private static final String VIEW_EDIT="producto/edit"; 
     private static String MODEL_PRODUCTO="producto";
-    //private final ProductoRepository productsData;
+    private final ProductoRepository productsData;
     
-
+    public ProductoController(ProductoRepository productsData
+    ){
+    this.productsData = productsData;
+    
+}      
 
     @GetMapping("/producto/index")
     public String index(Model model){
-       // List<Producto> listProducto = this.productsData.findAll();
-      //  model.addAttribute("productos",listProducto);
+       List<Producto> listProducto = this.productsData.findAll();
+      model.addAttribute("productos",listProducto);
         return VIEW_INDEX;
     }    
 
@@ -44,7 +48,7 @@ public class ProductoController {
             model.addAttribute("mensaje", "No se registro un Producto");
         }else{
             objProducto.setStatus("A");
-            //this.productsData.save(objProducto);
+            this.productsData.save(objProducto);
             model.addAttribute(MODEL_PRODUCTO, objProducto);
             model.addAttribute("mensaje", "Se registro un Producto");
         }
@@ -52,10 +56,10 @@ public class ProductoController {
     }
 
     @GetMapping("/producto/edit/{id}")
-    public String edit(@PathVariable("id") int id, 
+    public String edit(@PathVariable("id") Integer id, 
         Model model){
-        //Producto producto = this.productsData.getOne(id);
-        //model.addAttribute(MODEL_PRODUCTO, producto);
+        Producto producto = this.productsData.getOne(id);
+        model.addAttribute(MODEL_PRODUCTO, producto);
         return VIEW_EDIT;
     }  
 
@@ -67,7 +71,7 @@ public class ProductoController {
 		if(bindingResult.hasFieldErrors()) {
 			return "redirect:/producto/edit/{id}";
 		}
-		//this.productsData.save(objProducto);
+		this.productsData.save(objProducto);
 		return VIEW_INDEX;
 	}
 
